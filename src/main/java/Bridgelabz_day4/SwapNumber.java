76b8090c27efcf0da493c0fc53628f80cefeebf0
dinesh nangare <dineshnangare1717@gmail.com>
2022-04-02 16:45:21 +0530

    swap number

---
+++ src/main/java/Bridgelabz_day4/SwapNumber.java
@@ -0,0 +1,20 @@
+package Bridgelabz_day4;
+
+public class SwapNumber {
+    public static void main(String[] args) {
+        int a=10, b=20;
+        System.out.println("--Before Swap---");
+        System.out.println("first number "+a);
+        System.out.println("second number "+b);
+
+        int temp;
+        temp=a;
+        a=b;
+        b=temp;
+
+        System.out.println("--After Swap--");
+        System.out.println("first number "+a);
+        System.out.println("second number "+b);
+
+    }
+}
