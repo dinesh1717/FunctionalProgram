773bf1554cbdea9a625b24c6b1f32d61d4a893e4
dinesh nangare <dineshnangare1717@gmail.com>
2022-04-02 17:03:27 +0530

    power of 2

---
+++ src/main/java/Bridgelabz_day4/Power.java
@@ -0,0 +1,28 @@
+package Bridgelabz_day4;
+import java.util.Scanner;
+
+public class Power {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any number");
+        int n=sc.nextInt();
+        power(n);
+
+    }
+    static void power(int n)
+    {
+        if(0<=n && n <31)
+        {
+            int pow=1;
+            for(int i=0;i<=n;i++)
+            {
+                System.out.println("2^"+i+"  "+pow);   //ans=Math.pow(2,power)
+                pow=2*pow;                             //System.out.println(power+" "+ans);
+
+            }
+        }
+        else
+            System.out.println("invalid number");
+    }
+}
