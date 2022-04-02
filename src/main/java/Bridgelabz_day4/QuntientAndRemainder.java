b747bcafa3a4c1ffb3e1fb99810b2c847bc3ac41
dinesh nangare <dineshnangare1717@gmail.com>
2022-04-02 16:39:20 +0530

    Qutient program

---
+++ src/main/java/Bridgelabz_day4/QuntientAndRemainder.java
@@ -0,0 +1,13 @@
+package Bridgelabz_day4;
+
+public class QuntientAndRemainder {
+    public static void main(String[] args) {
+        int dividend = 20, divisor = 5;
+
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        System.out.println("Quotient = " + quotient);
+        System.out.println("Remainder = " + remainder);
+    }
+}
