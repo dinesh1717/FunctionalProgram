ba36a9758b169f760a1d8af9a4ef6c1ed2353e2f
dinesh nangare <dineshnangare1717@gmail.com>
2022-04-02 16:59:38 +0530

    Harmonic number added

---
+++ src/main/java/Bridgelabz_day4/Harmonic.java
@@ -0,0 +1,24 @@
+package Bridgelabz_day4;
+import java.util.Scanner;
+
+public class Harmonic {
+    public static void main(String[] args) {
+
+        double result = 0, num = 0, j;
+        int n;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("enter the harmonic you want to find: ");
+        n = scan.nextInt();
+
+        // COMPUTATION
+        for (j = 1; j <= n; j++) {
+            num = num + (1 / j);
+            result = num;
+        }
+        System.out.println("the value of harmonic no. " + n + " is: " + result);
+
+    }
+
+
+}
