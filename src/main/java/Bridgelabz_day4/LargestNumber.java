7678e2657c3642c3c46bfcd55bb7d5e7bf741bcd
dinesh nangare <dineshnangare1717@gmail.com>
2022-04-02 16:52:24 +0530

    large number added

---
+++ src/main/java/Bridgelabz_day4/LargestNumber.java
@@ -0,0 +1,16 @@
+package Bridgelabz_day4;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        int a=40, b=50, c=20;
+
+        if(a>=b && a>=c)
+            System.out.println(a+" is the largest Number");
+
+        else if (b>=a && b>=c)
+            System.out.println(b+" is the largest Number");
+        else
+
+            System.out.println(c+" is the largest number");
+    }
+}
